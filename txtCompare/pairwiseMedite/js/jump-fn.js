8b95f0fa8cd57954c1f90c84d02ee44a09b404f9
PhilippeGambette <philippe.gambette@laposte.net>
2023-11-30 00:54:59 +0100

    :bug: (txtCompare) remove the code to avoid 404 error

--- txtCompare/pairwiseMedite/js/jump-fn.js
+++
@@ -1,9 +0,0 @@
-function openDir( form ) { 
-	index = form.fieldname.selectedIndex;
-	var newIndex = form.fieldname.options[index].value; 
-		if ( newIndex == 'false') { 
-			alert( "Choisissez un groupe de romans." );
-		} else {
-			 window.location.assign( newIndex );
-		}
-	}
